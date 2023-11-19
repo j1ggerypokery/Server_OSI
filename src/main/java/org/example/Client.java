c644b8063ec17a4c338462705689af665d3601b5
Valentina Klochkova <KlochkovaValentina11@yandex.ru>
2023-11-19 11:00:12 +0300

    initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", Server.LOCALHOST_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            out.println("Peter");
+            System.out.println(in.readLine());
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static final Integer LOCALHOST_PORT = 8080;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(LOCALHOST_PORT)) {
+            System.out.println("Started");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    final String name = in.readLine();
+                    System.out.println("New connection accepted");
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        }
+     catch (IOException e) {
+            throw new RuntimeException(e);
+     }
+    }
+}
